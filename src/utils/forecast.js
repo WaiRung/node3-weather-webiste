b8e50f317861594cd5849a56e6d75bb16a374880
WaiRung <67456708+WaiRung@users.noreply.github.com>
2021-04-01 23:04:09 +0700

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+const forecast = (lat, long, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=263819027ce6b7fc7a2c9822da76898d&query=" + lat + "," + long + "" + "&units=f";
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.success === false) {
+            callback('Location not exists', undefined)
+        } else {
+            const temp = body.current.temperature;
+            const feel = body.current.feelslike;
+            const desc = body.current.weather_descriptions[0]
+            callback(undefined, desc + ' It is currently ' + temp + ' degrees out. It feels like ' + feel + ' degrees out')
+        }
+    })
+}
+module.exports = forecast
